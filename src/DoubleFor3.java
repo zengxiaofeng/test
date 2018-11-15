ccd0c3b42d801f28acb41ba713bcc561676fdcbd
shaoyumeng <shaoyumeng@gikoosz.com>
2018-11-15 17:05:17 +0800

    sym by edit

---
+++ src/DoubleFor3.java
@@ -0,0 +1,13 @@
+package cn.sym.day04;
+
+public class DoubleFor3 {
+	public static void main(String[] args) {
+		int n = 0;
+		for(int i = 1; i <= 4; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(++n + " ");
+			}
+			System.out.println();
+		}
+	}
+}
